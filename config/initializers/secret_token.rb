d8713471e4fd1210ae6bdca2e340a7cc767631bf
vagrant <vagrant@precise32.(none)>
2013-03-14 21:55:37 +0000

    initial rails creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsVhoStripscriptsError::Application.config.secret_token = '19dc089ab1aa48916ef1f027b2b873b6e3a12521628d8424e34685abb5cc62bdf9307fe60a359e63fe473ba06cf6562f0280ec2db9e7c14b3d0e411621bedd79'
